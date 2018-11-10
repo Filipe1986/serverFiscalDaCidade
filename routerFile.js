f730266d1bc37ef8c796538a494c6f89e8fc97a3
Filipe Gonçalves <libertadores81@gmail.com>
2018-11-10 15:34:15 -0200

    Fazendo upload de fotos com multer na pasta uploads

--- routerFile.js
+++ routerFile.js
@@ -1,22 +1,25 @@
 const routerFile = require('express').Router();
-const Busboy = require('busboy');
-const fs = require('fs');
+//const Busboy = require('busboy');
+//const fs = require('fs');
 var multer = require('multer'); 
 
 
 var storage =   multer.diskStorage({
     destination: function (req, file, callback) {
-      callback(null, './uploads');
+      callback(null, 'uploads');
     },
     filename: function (req, file, callback) {
-      callback(null, file.fieldname + '-' + Date.now());
+        callback(null, `${file.fieldname}-${Date.now()}.${file.originalname}`);
     }
   });
 
   var upload = multer({ storage: storage });
 
-  routerFile.route('/upload1').post( upload.single('photos'), function (req, res, next) {
-    console.log(req.files)
+  routerFile.post('/upload1', upload.single('photos'), function (req, res, next) {
+    console.log('file');
+    console.log(req.file);
+    console.log('files');
+    console.log(req.files);
 
     return res.end("File is uploaded");
     if(err) {
@@ -47,47 +50,6 @@ var storage =   multer.diskStorage({
 
   });
   
-
-routerFile.route('/upload').post(function (req, res, next) {
-
-    const element1 = req.body.element1;
-
-    console.log(element1);
-    var busboy = new Busboy({ headers: req.headers });
-    busboy.on('finish', function () {
-        console.log('Upload finished');
-        // Your files are stored in req.files. In this case,
-        // you only have one and it's req.files.element2:
-        // This returns:
-        // {
-        //    element2: {
-        //      data: ...contents of the file...,
-        //      name: 'Example.jpg',
-        //      encoding: '7bit',
-        //      mimetype: 'image/png',
-        //      truncated: false,
-        //      size: 959480
-        //    }
-        // }
-        // Grabs your file object from the request.
-        const file = req.files.element2;
-        
-        fs.writeFile('outputImage.png', file, 'binary', function (err) {
-        
-            if (err) {throw err}
-            else {
-                console.log('The file has been saved!');
-                text = "The file has been saved!";
-                res.json({ text });
-            }
-          });
-
-
-    });
-    req.pipe(busboy);
-
-
-});
 
 
 module.exports = routerFile;
--- server.js
+++ server.js
@@ -2,23 +2,13 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
-const busboy = require('connect-busboy');
-const busboyBodyParser = require('busboy-body-parser');
-
 var router = require('./routers');
 var routersUsuario = require('./routersUsuario');
 var routerFile = require('./routerFile');
 
-
-
-
-
-  app.use(busboy());
-
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
 
-  app.use(busboyBodyParser());
 
   app.use('/api', routersUsuario);
   app.use('/api', router);
